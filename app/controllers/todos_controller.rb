de6f91c32b952a2a2b4404afdf4a89cb7d5b36e8
Lydias303 <shaferlydia@yahoo.com>
2015-05-02 17:49:56 -0600

    added create route and actions using strong params for todos and form to create a todo on the page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,17 @@
 class TodosController < ApplicationController
   def index
+    @todo = Todo.new
     @todos = Todo.all
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :task)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'todos/index'
 
   root 'todos#index'
-  resources :todos, only: [:index]
+  resources :todos, only: [:index, :create]
 end
