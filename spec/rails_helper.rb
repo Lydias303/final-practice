2fc52eeb649f43a484d1e41f35f02b8c4ecb4c3d
Lydias303 <shaferlydia@yahoo.com>
2015-05-02 17:59:00 -0600

    added todo factory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,8 +7,16 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  
+
   config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+  begin
+    DatabaseCleaner.start
+    FactoryGirl.lint
+  ensure
+    DatabaseCleaner.clean
+  end
 
   config.use_transactional_fixtures = true
 
