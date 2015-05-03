c44707ef8cfde34b238806b9e1aaaf474321deb9
Lydias303 <shaferlydia@yahoo.com>
2015-05-03 14:22:53 -0600

    allowing orign and credentials for outside ember app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_filter :add_allow_credentials_headers
+  
   protect_from_forgery with: :exception
+
+  def add_allow_credentials_headers
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
 end
