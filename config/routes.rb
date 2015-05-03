38f3c92a71ae1e931f68a3be701cf0bfadde6096
Lydias303 <shaferlydia@yahoo.com>
2015-05-02 20:26:19 -0600

    added namespaced routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   root 'todos#index'
   resources :todos, only: [:index, :create, :destroy, :update, :edit]
+  namespace :api do
+    namespace :v1 do
+      resources :todos, only: [:index]
+    end
+  end
 end
