9efa7e2daddc4b7f0faebe3c17740607a21331b3
Lydias303 <shaferlydia@yahoo.com>
2015-05-02 20:30:33 -0600

    index test passing, added index action

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,4 +1,8 @@
-class Api::V1::TodoControllerController < ApplicationController
+class Api::V1::TodosController < ApplicationController
   respond_to :json
 
+  def index
+    @todos = Todo.all
+    respond_with @todos
+  end
 end
--- spec/controllers/api/v1/todos_controller_spec.rb
+++ spec/controllers/api/v1/todos_controller_spec.rb
@@ -13,9 +13,9 @@ RSpec.describe Api::V1::TodosController, type: :controller do
 
       first_todo = todos.first
 
-      expect(responds.status).to eq(200)
-      expect(first_todo["title"]).too eq(todo.title)
-      expect(first_todo["id"]).too eq(todo.id)
+      expect(response.status).to eq(200)
+      expect(first_todo["title"]).to eq(todo.title)
+      expect(first_todo["id"]).to eq(todo.id)
     end
   end
 end
