caa1f9194363f6dd77adba24edd189d32f12b84b
Lydias303 <shaferlydia@yahoo.com>
2015-05-03 14:25:37 -0600

    missing end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   before_filter :add_allow_credentials_headers
-  
+
   protect_from_forgery with: :exception
 
   def add_allow_credentials_headers
     response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
+  end
 end
